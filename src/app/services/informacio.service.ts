a0424fe916394332bc18aaa268b27a352077751a
JMLA <juan-00014@hotmail.com>
2017-10-09 16:01:41 -0500

    servicio con firebase ok

--- src/app/services/informacio.service.ts
+++ src/app/services/informacio.service.ts
@@ -5,14 +5,30 @@ import { Http } from "@angular/http";
 export class InformacioService {
 
   info:any = {};
+  equipo:any[] = [];
   cargada:boolean = false;
+  cargada_nosotros:boolean = false;
 
   constructor(public http:Http) { 
+    this.public_carga_info();
+    this.carga_nosotros();
+  }
+
+  public_carga_info(){
     this.http.get("assets/data/info.pagina.json")
         .subscribe(data =>{
-          console.log(data.json());
+          //console.log(data.json());
           this.cargada = true;            
           this.info = data.json();
+    })
+  }
+
+  carga_nosotros(){
+    this.http.get("https://paginaweb-51036.firebaseio.com/equipo.json")
+        .subscribe(data =>{
+          console.log(data.json());
+          this.cargada_nosotros = true;            
+          this.equipo = data.json();
     })
   }
 
